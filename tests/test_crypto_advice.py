d44eb6e4b79c1aa77971137bbffe427aab19fd77
yuqi88 <snow34545@gmail.com>
2022-03-17 12:28:01 -0700

    add test for crypto-advice intent

---
+++ tests/test_crypto_advice.py
@@ -0,0 +1,55 @@
+import unittest
+import sys
+import os
+import json
+from flask import Flask
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "elonmusk"))
+
+from tests.mock_dialogflow_utils import *
+
+from elonmusk.main import cloud_function
+
+class TestCryptoAdvice(unittest.TestCase):
+
+    def test_dogecoin(self):
+        """Test that question about dogecoin has dogecoin in answer"""
+        app = Flask(__name__)
+
+        with app.app_context():
+            test_dogecoin = get_test_request("Is_it_time_to_buy_doge")
+            response = cloud_function(test_dogecoin)
+            result = json.loads(response.get_data(as_text=True))
+
+            self.assertTrue(
+                "dogecoin" in result["fulfillmentMessages"][0]["text"]["text"][0]
+            )
+
+    def test_portfolio(self):
+        """Test that question about portfolio has portfolio in answer"""
+        app = Flask(__name__)
+
+        with app.app_context():
+            test_portfolio = get_test_request("Should_I_get_a_portfolio")
+            response = cloud_function(test_portfolio)
+            result = json.loads(response.get_data(as_text=True))
+
+            self.assertTrue(
+                "portfolio" in result["fulfillmentMessages"][0]["text"]["text"][0]
+            )
+
+    def test_crypto(self):
+        """Test that question about portfolio has portfolio in answer"""
+        app = Flask(__name__)
+
+        with app.app_context():
+            test_crypto = get_test_request("Should_I_start_investing_in_crypto")
+            response = cloud_function(test_crypto)
+            result = json.loads(response.get_data(as_text=True))
+
+            self.assertTrue(
+                "test_crypto" in result["fulfillmentMessages"][0]["text"]["text"][0]
+            )
+
+if __name__ == "__main__":
+    unittest.main()
