c326c414ee37e3cbc1f08bbf6829a7fc2b086617
KafkaNoNeko <paula.wong-chung@alumni.ubc.ca>
2022-04-03 11:20:44 -0700

    implement google maps search

--- elonmusk/map.py
+++ elonmusk/map.py
@@ -4,12 +4,15 @@ def map_search(text):
     """Extracts query from user-entered text and returns the associated Google maps link"""
     try: 
         # remove the word "map" and keep other keywords
+        # this step is not really needed
         query = text.lower().replace(" map", "")
 
         # encode to url
         query_url = urllib.parse.quote(query)
 
-        link = ""
+        link = "https://www.google.com/maps/search/?api=1&query=" + query_url
+
+        result = "Here you go! \n" + link
     except:
-        link = "I can't find it right now, but why not take a look at my new tweets first?"
-    return link
+        result = "I can't find it right now, but why not take a look at my new tweets first?"
+    return result
